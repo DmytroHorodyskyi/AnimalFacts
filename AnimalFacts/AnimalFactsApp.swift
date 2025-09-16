59715825daae94fd74f6bec9d3e34ceabc740272
Dmytro Horodyskyi <dimagorodiskij@gmail.com>
2025-09-16 07:00:22 +0300

    Initial Commit

---
+++ AnimalFacts/AnimalFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFactsApp.swift
+//  AnimalFacts
+//
+//  Created by Dmytro Horodyskyi on 16.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
